76aaad84f274dbd02bfc8959054f35757ecffa26
yuanyuan123123 <su18371125379@163.com>
2019-06-15 21:04:58 +0800

    我把工程myrepositoryfour提交到远程

---
+++ src/my/TestTwo.java
@@ -0,0 +1,10 @@
+package my;
+
+/**
+ * Created by Administrator on 2019/6/15.
+ */
+public class TestTwo {
+    public static void main(String args[]){
+        System.out.print("Hello!");
+    }
+}
