f73a941dfb45ecfca6395455fc7950ff61435e58
yuanyuan123123 <su18371125379@163.com>
2019-06-15 21:18:45 +0800

    这是myrepositoryfour工程添加类后的提交

---
+++ src/my/AddOne.java
@@ -0,0 +1,11 @@
+package my;
+
+/**
+ * Created by Administrator on 2019/6/15.
+ */
+public class AddOne {
+
+    public static void main(String args[]){
+        System.out.print("这是一个AddOne类的输出语句。");
+    }
+}
