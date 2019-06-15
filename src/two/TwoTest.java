98b7991c1758ff38d3079a022451511319a7ef5a
yuanyuan123123 <su18371125379@163.com>
2019-06-15 21:25:30 +0800

    这是myrepositoryfour工程添加two包、TwoTest类后的提交

---
+++ src/two/TwoTest.java
@@ -0,0 +1,10 @@
+package two;
+
+/**
+ * Created by Administrator on 2019/6/15.
+ */
+public class TwoTest {
+    public static void main(String args[]){
+        System.out.print("这是two包下的TwoTest类的输出语句！");
+    }
+}
