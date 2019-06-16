83a16fae531f6ef038861fe3940e506cddeeecd6
yuanyuan123123 <su18371125379@163.com>
2019-06-16 15:21:56 +0800

    这是myrepositoryfour工程添加five包、MyFiveTest类后的提交

---
+++ src/five/MyFiveTest.java
@@ -0,0 +1,11 @@
+package five;
+
+/**
+ * Created by Administrator on 2019/6/16.
+ */
+public class MyFiveTest {
+
+    public static void main(String args[]){
+        System.out.print("这是five包下的MyFiveTest类的输出语句！");
+    }
+}
