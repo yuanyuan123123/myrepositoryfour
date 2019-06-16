f532058dae9f22e097517683bc4ab07bfd9baf4b
yuanyuan123123 <su18371125379@163.com>
2019-06-16 15:29:16 +0800

    这是myrepositoryfour工程添加six包、MySixTest类后的提交

---
+++ src/six/MySixTest.java
@@ -0,0 +1,11 @@
+package six;
+
+/**
+ * Created by Administrator on 2019/6/16.
+ */
+public class MySixTest {
+
+    public static void main(String args[]){
+        System.out.print("这是six包下的MySixTest类的输出语句！");
+    }
+}
