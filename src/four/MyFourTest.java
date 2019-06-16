1352342c2ff83adb12ed616d328b51a404f0f1c7
yuanyuan123123 <su18371125379@163.com>
2019-06-16 15:03:18 +0800

    这是myrepositoryfour工程添加four包、MyFourTest类后的提交

---
+++ src/four/MyFourTest.java
@@ -0,0 +1,10 @@
+package four;
+
+/**
+ * Created by Administrator on 2019/6/16.
+ */
+public class MyFourTest {
+    public static void main(String args[]){
+        System.out.print("这是Four包下的MyFourTest类的输出语句！");
+    }
+}
