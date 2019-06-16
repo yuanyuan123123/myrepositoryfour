1bfcd41b2594d32f101ec8d202aa22c9bdb484d9
yuanyuan123123 <su18371125379@163.com>
2019-06-16 15:52:30 +0800

    这是myrepositoryfour工程添加seven包、MySevenTest类后的提交

---
+++ src/seven/MySevenTest.java
@@ -0,0 +1,11 @@
+package seven;
+
+/**
+ * Created by Administrator on 2019/6/16.
+ */
+public class MySevenTest {
+
+    public static void main(String args[]){
+        System.out.print("这是seven包下的MySevenTest类的输出语句！");
+    }
+}
