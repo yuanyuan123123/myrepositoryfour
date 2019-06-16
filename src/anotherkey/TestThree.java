1cd0b8c1661b505694e3cac0c39cc5962bdd1bea
Administrator <3107237076@qq.com>
2019-06-16 10:50:18 +0800

    这是myrepositoryfour工程添加anotherkey包、TestThree类后的提交

---
+++ src/anotherkey/TestThree.java
@@ -0,0 +1,14 @@
+package anotherkey;
+
+/**
+ * @program: future-emr-edas-root
+ * @description:
+ * @author: suihaiying
+ * @date: 2019/6/16 10:42
+ */
+public class TestThree {
+    public static void main(String args[]){
+        System.out.print("这是another包下的TestThree类的输出语句！");
+
+    }
+}
