a060a2b77b192ed90e502327aed7566bdd40b1d9
yuanyuan123123 <su18371125379@163.com>
2019-06-16 11:04:08 +0800

    这是myrepositoryfour工程添加three包、MyThreeTest类后的提交

---
+++ src/three/MyThreeTest.java
@@ -0,0 +1,10 @@
+package three;
+
+/**
+ * Created by Administrator on 2019/6/16.
+ */
+public class MyThreeTest {
+    public static void main(String args[]){
+        System.out.print("这是three包下的MyThreeTest类的输出语句！");
+    }
+}
