93217ae7c04138751e32da2a52de551d51d459fa
suihaiying <suihaiying@jhemr.com>
2020-05-17 19:51:55 +0800

    three包下的MyThreeTest类修改out

--- src/three/MyThreeTest.java
+++ src/three/MyThreeTest.java
@@ -6,5 +6,7 @@ package three;
 public class MyThreeTest {
     public static void main(String args[]){
         System.out.print("这是three包下的MyThreeTest类的输出语句！Hello");
+        int three=3;
+        System.out.println(three);
     }
 }
