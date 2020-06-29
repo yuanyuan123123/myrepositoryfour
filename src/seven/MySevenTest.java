94c6058b92db15726d375a6c366365a8b22f3f41
suihaiying <suihaiying@jhemr.com>
2020-06-29 10:07:16 +0800

    提交

--- src/seven/MySevenTest.java
+++ src/seven/MySevenTest.java
@@ -8,5 +8,7 @@ public class MySevenTest {
     public static void main(String args[]){
         System.out.println("这是seven包下的MySevenTest类的输出语句！");
         System.out.println("11");
+
+        System.out.println("456");
     }
 }
