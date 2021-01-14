a43f09e918a896da2380624c9d6604a30be196b8
suihaiying <suihaiying@jhemr.com>
2021-01-14 16:44:56 +0800

    master分支新增ClassesThree类

---
+++ src/four/ClassesThree.java
@@ -0,0 +1,22 @@
+package four;
+
+/**
+ * @ProjectName: myrepositoryfour
+ * @Package: four
+ * @ClassName: ClassesThree
+ * @Author: suihaiying
+ * @Description:
+ * @createDate 2021/01/14 16:44
+ */
+public class ClassesThree {
+
+    private String three;
+
+    public String getThree() {
+        return three;
+    }
+
+    public void setThree(String three) {
+        this.three = three;
+    }
+}
