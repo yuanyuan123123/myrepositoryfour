1e2071a61c10ad570f964009845dbf7cee8ee29d
suihaiying <suihaiying@jhemr.com>
2021-01-14 16:21:13 +0800

    master分支新增ClassesOne类

---
+++ src/five/ClassesOne.java
@@ -0,0 +1,22 @@
+package five;
+
+/**
+ * @ProjectName: myrepositoryfour
+ * @Package: five
+ * @ClassName: Classes
+ * @Author: suihaiying
+ * @Description:
+ * @createDate 2021/01/14 16:19
+ */
+public class ClassesOne {
+
+    private String b;
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+}
