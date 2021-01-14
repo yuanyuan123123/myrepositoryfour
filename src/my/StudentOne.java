f10071cb29e15749232c4ff3985ec3ea2ef236e3
suihaiying <suihaiying@jhemr.com>
2021-01-14 16:14:12 +0800

    新增dev_2分支

---
+++ src/my/StudentOne.java
@@ -0,0 +1,22 @@
+package my;
+
+/**
+ * @ProjectName: myrepositoryfour
+ * @Package: my
+ * @ClassName: StudentOne
+ * @Author: suihaiying
+ * @Description:
+ * @createDate 2021/01/14 16:13
+ */
+public class StudentOne {
+
+    private String a;
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+}
